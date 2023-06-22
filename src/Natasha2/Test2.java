ca4470f7fcf85506f600126605be6bac7f59598c
Nat3642 <123883005+Nat3642@users.noreply.github.com>
2023-06-22 09:51:00 -0600

    Test edited again

--- src/Natasha2/Test2.java
+++ src/Natasha2/Test2.java
@@ -3,6 +3,6 @@ package Natasha2;
 public class Test2 {
     public static void main(String[] args) {
         System.out.println("Hello" +
-                " World");
+                " Kenan");
     }
 }
