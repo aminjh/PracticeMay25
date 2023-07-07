604b220d18b67abbee104869fab51adc11d06652
Natalya_K <natkhvan@gmail.com>
2023-07-06 21:19:11 -0500

    added IQs

--- src/Conflict1/conflictPractice1.java
+++ src/Conflict1/conflictPractice1.java
@@ -1,7 +1,10 @@
 package Conflict1;
 
 public class conflictPractice1 {
-
+    // Interview Questions:
+    // 1. Tell me about you.
+    // 2. Tell me about your framework.
+    // 3. How do you implement Agile framework?
 
 
 
